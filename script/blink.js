00b80038861f94be45f4c1cd9cfd1ad7848aef6a
Nisarg Shah <49806524+nisargshah96@users.noreply.github.com>
2020-04-18 12:08:13 +0530

    Made Blink Brand Name Working

--- script/blink.js
+++ script/blink.js
@@ -1,13 +1,6 @@
-function blink()
-{
-    let name = document.querySelector("span.navbar fixed-top navbar-expand-lg navbar-dark bg-darks nav.navbar fixed-top navbar-expand-lg navbar-dark bg-dark");
-    if(name.style.visibiliy === 'hidden')
-    {
-        name.style.visibiliy = 'visible';
-    }
-    else{
-        name.style.visibiliy = 'hidden';
-    }
-
+function blink() {
+  let bname = document.getElementById("b_name");
+  setInterval(function () {
+    bname.style.display = bname.style.display == "none" ? "" : "none";
+  }, 1000);
 }
-window.setInterval(blink,500);
