81e19bf74b2b93a71972227385b57bae409c752e
Nisarg Shah <49806524+nisargshah96@users.noreply.github.com>
2020-04-19 12:30:43 +0530

    Delete blink.js

--- script/blink.js
+++
@@ -1,6 +0,0 @@
-function blink() {
-  let bname = document.getElementById("b_name");
-  setInterval(function () {
-    bname.style.display = bname.style.display == "none" ? "" : "none";
-  }, 1000);
-}
